d45e62b0fd4170b5527483451cfbe971c19da8c7
Davide Pesavento <davide.pesavento@lip6.fr>
2018-01-01 23:33:18 -0500

    examples: add missing include
    Change-Id: I0d5d3c9a710d4d660605a3c4ba7ccdac133e6add

--- examples/data-producer.cpp
+++ examples/data-producer.cpp
@@ -1,6 +1,6 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /*
- * Copyright (c) 2014-2017, Regents of the University of California.
+ * Copyright (c) 2014-2018, Regents of the University of California.
  *
  * This file is part of NDN repo-ng (Next generation of NDN repository).
  * See AUTHORS.md for complete list of repo-ng authors and contributors.
@@ -35,6 +35,7 @@
 
 #include "../src/common.hpp"
 
+#include <boost/asio/io_service.hpp>
 #include <boost/lexical_cast.hpp>
 #include <fstream>
 #include <iostream>
